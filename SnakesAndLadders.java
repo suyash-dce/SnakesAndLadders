6eb5a897ef9ba949f784a4456ffbe79f47bc8473
Suyash Jain <jain.suyash28@gmail.com>
2020-09-25 11:32:36 +0530

    Added: Dice Roll Functionality is Added

--- SnakesAndLadders.java
+++ SnakesAndLadders.java
@@ -2,5 +2,6 @@ public class SnakesAndLadders {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub	
 		System.out.println("Let the Game Begin !!");
+		int roll = (int) (Math.floor((Math.random()*6))+1);
 		}
 	}
