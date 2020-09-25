f1e0f1d71a1b9f3509e900e0b2085d05a568352c
Suyash Jain <jain.suyash28@gmail.com>
2020-09-25 11:38:14 +0530

    Added: Movement Check Ladder/Snake is Added

--- SnakesAndLadders.java
+++ SnakesAndLadders.java
@@ -1,6 +1,22 @@
 public class SnakesAndLadders {
+	public static final int IS_LADDER=0;
+	public static final int IS_SNAKE=1;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub	
 		System.out.println("Let the Game Begin !!");
+		int pos =0;
+		int CheckChance = (int) (Math.floor(Math.random()*10)%3);
+			
+			switch(CheckChance) {
+				case  IS_LADDER :
+					pos = pos+roll;
+					break;
+				case IS_SNAKE :
+					pos = pos-roll;
+					break;
+				default:
+					pos=pos;	
+					
+			}
 		}
 	}
