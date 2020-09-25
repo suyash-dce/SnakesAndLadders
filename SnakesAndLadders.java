8f6ec9bfb5018c41cd611e876397073c62284bc8
Suyash Jain <jain.suyash28@gmail.com>
2020-09-25 11:29:05 +0530

    Added: Start Position is Added

--- SnakesAndLadders.java
+++ SnakesAndLadders.java
@@ -2,5 +2,7 @@ public class SnakesAndLadders {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub	
 		System.out.println("Let the Game Begin !!");
+		//Variables
+		int pos=0;
 		}
 	}
